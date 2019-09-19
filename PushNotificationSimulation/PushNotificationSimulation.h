e7d50f3827141f020a6cc675c6a6b5fcd9f1f7d2
Paweł Kania <pkania@pgs-soft.com>
2019-09-19 11:50:28 +0200

    Initial project setup

---
+++ PushNotificationSimulation/PushNotificationSimulation.h
@@ -0,0 +1,18 @@
+//
+//  PushNotificationSimulation.h
+//  PushNotificationSimulation
+//
+//  Created by Pawel Kania on 19/09/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PushNotificationSimulation.
+FOUNDATION_EXPORT double PushNotificationSimulationVersionNumber;
+
+//! Project version string for PushNotificationSimulation.
+FOUNDATION_EXPORT const unsigned char PushNotificationSimulationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PushNotificationSimulation/PublicHeader.h>
+
+
---
+++ PushNotificationSimulationExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PushNotificationSimulationExamples
+//
+//  Created by Pawel Kania on 19/09/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
